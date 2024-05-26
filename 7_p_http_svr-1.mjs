5c11350703ba049b548dfed4e9107f2cfffca803
joechea <s.chea@aupp.edu.kh>
2024-05-26 15:21:02 +0700

    add and modify point 7 with part 1 and part 2

--- 7_p_http_svr-1.mjs
+++ 7_p_http_svr-1.mjs
@@ -25,33 +25,6 @@ http
     } else if (req.method === "GET") {
       const parse = url.parse(req.url, true);
       console.log(parse.query);
-    } else if (req.method === "PUT") {
-      let body = "";
-      req.on("data", (data) => {
-        body += data;
-      });
-      req.on("end", () => {
-        body = JSON.parse(body);
-        const index = users.findIndex(
-          (user) => user.username === body.username,
-        );
-        if (index !== -1) {
-          users[index] = { ...body };
-          res.write("user updated");
-        }
-        console.log(users);
-      });
-    } else if (req.method === "DELETE") {
-      let body = "";
-      req.on("data", (data) => {
-        body += data;
-      });
-      req.on("end", () => {
-        body = JSON.parse(body);
-        users = users.filter((user) => user.username !== body.username);
-
-        console.log(users);
-      });
     }
 
     res.end();
